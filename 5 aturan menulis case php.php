fc76d395f19f25d5b7e2e56e1b10637cf2b59f21
mafriansyah <ryanksp34@gmail.com>
2024-08-27 13:41:00 +0700

    5 aturan menulis case php

---
+++ 5 aturan menulis case php.php
@@ -0,0 +1,25 @@
+<?php
+
+$nama = "M AFRIANSYAH";
+$NAMA = "RUSLI";
+$UMUR = 16;
+const INI_KONSTANTA = 123;
+$ini_variabel = 30;
+$iniJugaVariabel = 15;
+$iniJugaVariabel;
+
+echo " ==== mencetak variabel ==== ";
+echo "<p>nama saya adalah $nama";
+echo "<p>nama kawan saya adalah $nama";
+echo "<p>umur saya adalah $umur";
+
+echo
+
+    "<p>coba pakai blok program",
+    "<p>nama saya adalah $nama",
+    "<p>nama kawan saya adalah adalah $nama",
+    "<p>umur saya adalah $umur";
+
+    echo "<br><br><br><strong>by M AFRIANSYAH";
+?>
+
