e941ece427970aeebff46895de9ac5fd6fe9e09a
mafriansyah <ryanksp34@gmail.com>
2024-08-27 13:05:08 +0700

    3 menulis php dalam, html

---
+++ 3 menulis php dalam html.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "<html>";
+echo "<head>";
+echo "<title>Judul Web</title>";
+echo "</head>";
+echo "<body>";
+echo "<h1>Selamat datang</h1>";
+echo "di smk 1 karang baru aceh tamiang";
+echo "</body>";
+echo "</html>";
+echo "<br><br><br><strong>By M AFRIANSYAH";
