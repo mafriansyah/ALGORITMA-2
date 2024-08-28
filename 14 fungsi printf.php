fb7147567982112096090a80439725cdefbb516e
mafriansyah <ryanksp34@gmail.com>
2024-08-28 11:23:06 +0700

    14 fungsi printf

--- 14 fungsi printf.php
+++ 14 fungsi printf.php
@@ -1,3 +1,11 @@
-$txt = "ahmadimuslim";
-printf("Dibelajar pemrograman PHP di %s<br>", $txt);
+<?php
+// misalkan kita punya bilangan dengan notasi E seperti ini
+$harga = 500000;
 
+// jika kita cetak dengan echo:
+echo "Harganya adalah Rp $harga";
+
+// jika kita cetak dengan printf
+printf("<p>Harganya adalah Rp %.2f", $harga);
+
+echo "<br><br><br><strong>By mafriansyah</strong>";
