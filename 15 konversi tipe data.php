e3364f1dedc99620122f05c8acfe0d2441cfa9f1
mafriansyah <ryanksp34@gmail.com>
2024-08-28 11:19:51 +0700

    15 konversi tipe data

--- 15 konversi tipe data.php
+++ 15 konversi tipe data.php
@@ -1,4 +1,7 @@
-$a = 3;
-$b = "angka 10";
-
-$c = $b / $a;
+<?php
+$var = "123.45";
+$intvar = "123";
+$intvar = intval($var);//mengubah menjadi integer
+$floatvar = floatval($var);// mengubah menjadi float
+echo $intvar;//output:123
+echo "<p>$floatvar";//output:123.45
