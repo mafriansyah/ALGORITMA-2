362265ffc372d2d5be9ee51fe0808e80a7663b1f
mafriansyah <ryanksp34@gmail.com>
2024-08-28 11:46:20 +0700

    23 tipe data bolean

--- 23 tipe data bolean.php
+++ 23 tipe data bolean.php
@@ -1,2 +1,19 @@
+<?php
+//mendeklarasikan variable boolean 
 $isActive = false;
 $menikah = true;
+//menggunakan boolean dalam peryataan if
+if ($isActive){
+    echo "Nilai ini benar.";
+} else{
+    echo "Nilai ini salah.";
+}
+
+//contoh lain mnggunakan boolean 
+$condition = (5 > 2);
+if ($condition) {
+    echo "<p>5 lebih besar dari 2.";
+} else {
+    echo "5 tidak lebih besar dari 2.";
+}
+echo "<br><br><br><strong>By mafriansyah</strong>";
